3b3d6e32f96b8293acd04be932f88b4002639048
KarelVDW <karel.van.de.winkel@gmail.com>
2026-04-19 17:42:37 +0200

    fix: bug in Resizer

--- apps/web/src/model/util/Resizer.ts
+++ apps/web/src/model/util/Resizer.ts
@@ -1,16 +1,8 @@
-import { sortBy, sum, sumBy, uniq } from 'lodash-es'
+import { sum, sumBy } from 'lodash-es'
 
 class ResizeError extends Error {
     constructor() {
         super('Container too small')
-
-        //   JSON.stringify({
-        //                         measureWidth: this.measureWidth,
-        //                         minimalWidth: this.measure.minimalWidth,
-        //                         physical: sumBy(this.measure.physicalElements, el => el.width.total),
-        //                         totalReserve,
-        //                         totalDeficit,
-        //                     }),
     }
 }
 
@@ -33,21 +25,12 @@ export class Resizer<T extends Sizeable> {
         const totalMinimums = sumBy(this.elements, (el) => el.minimum)
 
         let totalWidth: number
-        if (typeof options?.width === 'number') {
-            console.log('totalWidth from options')
-            totalWidth = options.width
-        } else if (typeof options?.maximumWidth === 'number') {
-            console.log('totalWidth from maximumWidth')
+        if (typeof options?.width === 'number') totalWidth = options.width
+        else if (typeof options?.maximumWidth === 'number')
             totalWidth = Math.min(Math.max(totalDefaults, totalMinimums), options.maximumWidth)
-        } else {
-            console.log('totalWidth from defaults')
-            totalWidth = totalDefaults
-        }
+        else totalWidth = totalDefaults
 
-        if (totalMinimums - 0.0001 > totalWidth) {
-            console.log('elements', this.elements)
-            throw new ResizeError()
-        }
+        if (totalMinimums - 0.0001 > totalWidth) throw new ResizeError()
 
         const correction = this.elements.length > 0 ? (totalWidth - totalDefaults) / this.elements.length : 0
         for (const sizeable of this.elements) {
@@ -56,44 +39,17 @@ export class Resizer<T extends Sizeable> {
             if (allotedWidth < sizeable.minimum) deficits.set(sizeable, sizeable.minimum - allotedWidth)
             if (allotedWidth > sizeable.minimum) reserves.set(sizeable, allotedWidth - sizeable.minimum)
         }
-        let totalDeficit = sum(Array.from(deficits.values()))
 
         if (deficits.size) {
-            const sortedReserves = sortBy(uniq(Array.from(reserves.values())))
-            let currentLevel = 0
-            for (const level of sortedReserves) {
-                const levelDiff = level - currentLevel
-                currentLevel = level
-                const totalPossibleFillIn = levelDiff * reserves.size
-                const actualFillIn = totalPossibleFillIn > totalDeficit ? totalDeficit : totalPossibleFillIn
-                totalDeficit -= actualFillIn
-                let remaining = actualFillIn
-
-                for (const [el, deficit] of Array.from(deficits)) {
-                    const give = Math.min(deficit, remaining / deficits.size)
-                    if (deficit <= give) {
-                        deficits.delete(el)
-                    } else {
-                        deficits.set(el, deficit - give)
-                    }
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                    allotted.set(el, allotted.get(el)! + give)
-                    remaining -= give
-                }
-
-                const reserveIncrement = actualFillIn / reserves.size
-                for (const [el, reserve] of Array.from(reserves)) {
-                    if (reserve <= reserveIncrement) {
-                        reserves.delete(el)
-                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                        allotted.set(el, allotted.get(el)! - reserve)
-                    } else {
-                        reserves.set(el, reserve - reserveIncrement)
-                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                        allotted.set(el, allotted.get(el)! - reserveIncrement)
-                    }
-                }
-                if (actualFillIn < totalPossibleFillIn) break
+            const totalDeficit = sum(Array.from(deficits.values()))
+            const totalReserve = sum(Array.from(reserves.values()))
+            for (const el of deficits.keys()) {
+                allotted.set(el, el.minimum)
+            }
+            for (const [el, reserve] of reserves) {
+                const take = (reserve / totalReserve) * totalDeficit
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                allotted.set(el, allotted.get(el)! - take)
             }
         }
         this._sizeMap = allotted
